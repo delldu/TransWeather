676791dbfdd377cce6909dc1b55355282259d3bf
delldu <18588220928@163.com>
2022-09-26 02:27:25 +0800

    Update

--- project/demo.py
+++ project/demo.py
@@ -9,5 +9,4 @@ import image_derain
 # image_derain.image_predict("images/input/*.png", "output")
 # image_derain.video_predict("/home/dell/noise.mp4", "output/predict_clean.mp4")
 
-image_derain.image_predict("light_rain_images/input/*.png", "output/light")
-image_derain.image_predict("heavy_rain_images/input/*.png", "output/heavy")
+image_derain.image_predict("images/*.png", "output")
